9a611e5c8127ab679f4c15f5e9fad5f9cd11e8d3
hitode909 <hitode909@gmail.com>
2014-03-09 00:15:25 +0900

    define create_hello_world method, test helper

--- t/test.pm
+++ t/test.pm
@@ -7,6 +7,15 @@ use utf8;
 use Path::Class;
 use lib file(__FILE__)->dir->parent->subdir('lib')->stringify;
 
+use File::Temp qw(tempdir);
+use File::Copy::Recursive;
+
+# use Exporter::Lite ();
+
+our @EXPORT = qw(
+    create_hello_world
+);
+
 sub import {
     my ($class) = @_;
 
@@ -26,6 +35,13 @@ sub import {
 
     eval $code;
     die $@ if $@;
+}
+
+sub create_hello_world {
+    my $tmpdir = tempdir;
+
+    File::Copy::Recursive::dircopy(file(__FILE__)->dir->subdir('data', 'hello_world'), $tmpdir);
+    $tmpdir;
 }
 
 1;
