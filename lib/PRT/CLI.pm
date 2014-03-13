7cb1fec584b83577b77ba197106688dc0d7af17a
hitode909 <hitode909@gmail.com>
2014-03-14 00:49:40 +0900

    making CLI interface

--- lib/PRT/CLI.pm
+++ lib/PRT/CLI.pm
@@ -2,6 +2,7 @@ package PRT::CLI;
 use strict;
 use warnings;
 
+use Class::Load qw(load_class);
 use Getopt::Long qw(GetOptionsFromArray);
 
 sub new {
@@ -10,12 +11,48 @@ sub new {
     bless {}, $class;
 }
 
-sub run {
-    my ($class, @args) = @_;
+sub parse {
+    my ($self, @args) = @_;
 
     my $command = shift @args || 'help';
 
-    warn $command;
+    my $command_class = $self->_command_name_to_command_class($command);
+    load_class $command_class;
+    $self->{command} = $command_class->new;
+
+    my $collector_name = 'files';
+    my $collector_class = $self->_collector_name_to_collector_class($collector_name);
+    load_class $collector_class;
+
+    $self->{collector} = $collector_class->new;
+}
+
+sub command {
+    my ($self) = @_;
+
+    $self->{command};
+}
+
+sub collector {
+    my ($self) = @_;
+
+    $self->{collector};
+}
+
+sub _command_name_to_command_class {
+    my ($self, $name) = @_;
+
+    my $command_class = join '', map { ucfirst } split '_', $name;
+
+    'PRT::Command::' . $command_class;
+}
+
+sub _collector_name_to_collector_class {
+    my ($self, $name) = @_;
+
+    my $command_class = join '', map { ucfirst } split '_', $name;
+
+    'PRT::Collector::' . $command_class;
 }
 
 1;
--- t/PRT-CLI.t
+++ t/PRT-CLI.t
@@ -11,11 +11,23 @@ sub instantiate : Tests {
     isa_ok PRT::CLI->new, 'PRT::CLI';
 }
 
-sub run : Tests {
+sub _command_name_to_command_class : Tests {
     my $cli = PRT::CLI->new;
 
-    ok ! exception {
-        $cli->run('help');
-    }, 'can run';
+    is $cli->_command_name_to_command_class('hello'), 'PRT::Command::Hello', 'ucfirst';
+    is $cli->_command_name_to_command_class('replace_token'), 'PRT::Command::ReplaceToken', 'separate by _';
+}
 
+sub _collector_name_to_collector_class : Tests {
+    my $cli = PRT::CLI->new;
+
+    is $cli->_collector_name_to_collector_class('files'), 'PRT::Collector::Files', 'ucfirst';
+    is $cli->_collector_name_to_collector_class('git_directory'), 'PRT::Collector::GitDirectory', 'separate by _';
+}
+
+sub parse : Tests {
+    my $cli = PRT::CLI->new;
+    $cli->parse;
+    isa_ok $cli->command, 'PRT::Command::Help', 'Help command loaded';
+    isa_ok $cli->collector, 'PRT::Collector::Files', 'Files collector loaded';
 }
