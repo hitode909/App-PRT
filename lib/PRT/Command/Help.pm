1e82787458e4ee31b73bc7c2744b47355216e4fe
hitode909 <hitode909@gmail.com>
2014-03-22 20:58:49 +0900

    readme

--- lib/PRT/Command/Help.pm
+++ lib/PRT/Command/Help.pm
@@ -24,7 +24,13 @@ sub execute {
 
 sub help_message {
     return <<HELP;
-usage: prt <command> <args>
+usage: prt <command> <args> <files>
+
+Examples:
+  prt replace_token foo bar *.pm
+    replace tokens with content 'foo' with 'bar' in *.pm.
+  prt rename_class Foo Bar lib/*.pm
+    Rename Foo class to Bar. This command will rename lib/Foo.pm to lib/Bar.pm.
 HELP
 }
 
